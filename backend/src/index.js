a37734187c2418c7e675d640f612968653d51a62
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-22 21:22:59 +0500

    working on databse setup

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,14 @@
 import express from "express";
 import authRoutes from "./routes/auth.route.js";
+import dotenv from "dotenv";
 
 const app = express();
 
+dotenv.config({path: ".env.production"})
+const PORT = process.env.PORT;
+
 app.use("/api/auth", authRoutes);
 
-app.listen(5001, () => {
-    console.log("Server listening on 5001"); 
+app.listen(PORT, () => {
+    console.log("Server listening on, ", PORT); 
 })

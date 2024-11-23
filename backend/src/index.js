bd1164bf71d5cadc1a35b7584a7f57ef67d1c3e2
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-23 11:42:11 +0500

    login, logout controllers done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,16 +2,18 @@ import express from "express";
 import authRoutes from "./routes/auth.route.js";
 import serverConfig from "./config/serverConfig.js";
 import { connectDB } from "./lib/db.js";
+import cookieParser from "cookie-parser";
 
 const app = express();
 
 const PORT = serverConfig.PORT;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 
 app.listen(PORT, () => {
     console.log("Server listening on, ", PORT);
-    connectDB(); 
+    connectDB();
 })
---
+++ backend/src/middleware/auth.middleware.js
@@ -0,0 +1,39 @@
+import jwt from "jsonwebtoken";
+import User from "../models/user.model.js";
+import serverConfig from "../config/serverConfig.js";
+
+export const protectRoute = async (req, res, next) => {
+    try {
+        const token = res.cookie.jwt;
+
+        if (!token) {
+            return res.status(401).json({
+                message: "Not authorized - No Token Provided"
+            });
+        }
+
+        const decoded = jwt.verify(token, serverConfig.JWT_SECRET_KEY);
+
+        if (!decoded) {
+            return res.status(401).json({
+                message: "Not authorized - Invalid Token"
+            });
+        }
+
+        const user = await User.findById(decoded.userId).select("-password");
+
+        if (!user) {
+            return res.status(404).json({
+                message: "User not found"
+            });
+        }
+
+        req.user = user;
+        next();
+    } catch (error) {
+        console.log("Error in protectRoute middleware", error.message);
+        res.status(500).json({
+            message: "Internal Server Error"
+        });
+    }
+}

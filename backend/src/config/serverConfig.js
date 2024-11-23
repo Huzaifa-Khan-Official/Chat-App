0e35376aa399b8f1bf64c132e6654ea8ec4c4034
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-23 11:18:24 +0500

    signup controller done

--- backend/src/config/serverConfig.js
+++ backend/src/config/serverConfig.js
@@ -4,7 +4,9 @@ dotenv.config({ path: ".env.production" })
 
 const serverConfig = {
     PORT: process.env.PORT,
-    dbURI: process.env.MONGODB_URI
+    dbURI: process.env.MONGODB_URI,
+    JWT_SECRET_KEY: process.env.JWT_SECRET_KEY,
+    NODE_ENV: process.env.NODE_ENV
 }
 
 export default serverConfig;
--- backend/src/lib/utils.js
+++ backend/src/lib/utils.js
@@ -1,3 +1,18 @@
-export const generateToken = () => {
+import jwt from "jsonwebtoken"
+import serverConfig from "../config/serverConfig.js"
 
+
+export const generateToken = (userId, res) => {
+    const token = jwt.sign({ userId }, serverConfig.JWT_SECRET_KEY, {
+        expiresIn: "7d"
+    });
+
+    res.cookie("jwt", token, {
+        maxAge: 7 * 24 * 60 * 60 * 1000,
+        httpOnly: true,
+        sameSite: "strict",
+        secure: serverConfig.NODE_ENV !== "development"
+    });
+
+    return token;
 }

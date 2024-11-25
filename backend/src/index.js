b76a16dcbb7fa965552fe2331e43a5a490f1d2f1
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-25 17:01:13 +0500

    useAuth Store, Routes, and pages starter working done

--- backend/src/index.js
+++ backend/src/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import serverConfig from "./config/serverConfig.js";
 import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
+import cors from "cors";
 
 // Routes Imports
 import authRoutes from "./routes/auth.route.js";
@@ -13,6 +14,10 @@ const PORT = serverConfig.PORT;
 
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true
+}))
 
 app.use("/api/auth", authRoutes);
 app.use("/api/message", messageRoutes);

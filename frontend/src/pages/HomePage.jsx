7e38d9afc8237f5697514260a6bdcb9f40a8fedf
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-29 22:48:26 +0500

    Chat Container done

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -6,9 +6,9 @@ import { useChatStore } from "../store/useChatStore"
 const HomePage = () => {
   const { selectedUser } = useChatStore();
   return (
-    <div className="min-h-screen bg-base-200 pb-10">
+    <div className="h-screen bg-base-200">
       <div className="flex items-center   justify-center pt-20 px-4">
-        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc[100vh-8rem]]">
+        <div className="bg-base-100 rounded-lg shadow-xl w-full max-w-6xl h-[calc(100vh-6rem)]">
           <div className="flex h-full rounded-lg overflow-hidden">
             <Sidebar />
 

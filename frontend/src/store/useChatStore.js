31035ac813cf498bfa353574ec05815390f5e92e
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-30 00:36:59 +0500

    realtime messages work done

--- frontend/src/store/useChatStore.js
+++ frontend/src/store/useChatStore.js
@@ -1,6 +1,7 @@
 import { create } from "zustand";
 import toast from "react-hot-toast";
 import { axiosInstance } from "../lib/axios";
+import { useAuthStore } from "./useAuthStore"
 
 export const useChatStore = create((set, get) => ({
     messages: [],
@@ -34,13 +35,31 @@ export const useChatStore = create((set, get) => ({
     },
 
     sendMessage: async (messageData) => {
-        const {selectedUser, messages} = get();
+        const { selectedUser, messages } = get();
         try {
             const res = await axiosInstance.post(`/messages/send/${selectedUser._id}`, messageData);
-            set({messages: [...messages, res.data]});
+            set({ messages: [...messages, res.data] });
         } catch (error) {
             toast.error(error.response.data.message);
         }
+    },
+
+    subscribeToMessages: () => {
+        const { selectedUser } = get();
+        if (!selectedUser) return;
+
+        const socket = useAuthStore.getState().socket;
+
+        // todo: optimize this later
+        socket.on("newMessage", (newMessage) => {
+            set({ messages: [...get().messages, newMessage] });
+        });
+    },
+
+    unsubscribeFromMessages: () => {
+        const socket = useAuthStore.getState().socket;
+
+        socket.off("newMessage");
     },
 
     // todo: optimize this later

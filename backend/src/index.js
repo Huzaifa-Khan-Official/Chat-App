0146fe98e919f8e6cba7b04111210c9cb8f6f79c
Huzaifa Khan <m.huzaifakhan2004@gmail.com>
2024-11-30 10:13:14 +0500

    project done

--- backend/src/index.js
+++ backend/src/index.js
@@ -4,12 +4,15 @@ import { connectDB } from "./lib/db.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
 
+import path from "path";
+
 // Routes Imports
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
 import { app, server } from "./lib/socket.js";
 
 const PORT = serverConfig.PORT;
+const __dirname = path.resolve();
 
 app.use(express.json({ limit: "10mb" }));
 app.use(cookieParser());
@@ -20,6 +23,14 @@ app.use(cors({
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    })
+}
 
 server.listen(PORT, () => {
     console.log("Server listening on, ", PORT);
